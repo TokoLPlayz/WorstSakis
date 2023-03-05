8c16016b406ef7df886779d275e2ff22ce4cc31c
TokoLPlayz <110418462+TokoLPlayz@users.noreply.github.com>
2023-03-05 12:24:06 -0800

    Create login.js

---
+++ logedasdds/login.js
@@ -0,0 +1,21 @@
+function check() {
+  let password = document.getElementById('password').value;
+  let username = document.getElementById('username').value;
+  
+  const request = new XMLHttpRequest();
+  request.open('GET', '/config/config.json');
+  request.responseType = 'json';
+  request.send();
+  
+  request.onload = function() {
+    const users = request.response.users;
+    const realusername = users.username;
+    const realpassword = users.password;
+    if (password == realpassword && username == realusername) {
+        alert("Permission Granted!");
+        window.location.replace("https://WorstSakis.gervase.repl.co/logedasdds/page/")
+     } else {
+        alert("Username or Password is Incorrect!");
+     }
+  };
+}
